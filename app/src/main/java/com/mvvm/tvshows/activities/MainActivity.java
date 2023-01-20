bea2e64848de70f64d84405aad6e020092a56a65
musfique113 <musfique113@gmail.com>
2023-01-20 15:49:03 +0600

    show lists added

--- app/src/main/java/com/mvvm/tvshows/activities/MainActivity.java
+++ app/src/main/java/com/mvvm/tvshows/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mvvm.tvshows.activities;
 
         import androidx.appcompat.app.AppCompatActivity;
+        import androidx.databinding.DataBindingUtil;
         import androidx.lifecycle.ViewModel;
         import androidx.lifecycle.ViewModelProvider;
 
@@ -8,22 +9,51 @@ package com.mvvm.tvshows.activities;
         import android.widget.Toast;
 
         import com.mvvm.tvshows.R;
+        import com.mvvm.tvshows.adapters.TVShowsAdapter;
+        import com.mvvm.tvshows.databinding.ActivityMainBinding;
+        import com.mvvm.tvshows.models.TVShow;
         import com.mvvm.tvshows.viewmodels.MostPopularTVShowsViewModel;
+
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private MostPopularTVShowsViewModel viewModel;
+    private ActivityMainBinding activityMainBinding;
+    private List<TVShow> tvShows = new ArrayList<>();
+    private TVShowsAdapter tvShowsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
-        getMostPopularTVShows();
+        //link XML to java
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        doInitialization();
+      //  setContentView(R.layout.activity_main);
+
 
     }
 
+    public void doInitialization(){
+        activityMainBinding.tvShowsRecyclerView.setHasFixedSize(true);
+        viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        tvShowsAdapter = new TVShowsAdapter(tvShows);
+        activityMainBinding.tvShowsRecyclerView.setAdapter(tvShowsAdapter);
+        getMostPopularTVShows();
+    }
     private void getMostPopularTVShows() {
-        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse -> Toast.makeText(getApplicationContext(), "Total Pages" + mostPopularTVShowsResponse.getTotalPages(), Toast.LENGTH_SHORT).show());
+        activityMainBinding.setIsLoading(true);
+        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse ->{
+            activityMainBinding.setIsLoading(false);
+            if (mostPopularTVShowsResponse != null) {
+                if (mostPopularTVShowsResponse.getTvShows() != null){
+                    tvShows.addAll(mostPopularTVShowsResponse.getTvShows());
+                    tvShowsAdapter.notifyDataSetChanged();
+                }
+
+            }
+            // Toast.makeText(getApplicationContext(), "Total Pages" + mostPopularTVShowsResponse.getTotalPages(), Toast.LENGTH_SHORT).show()
+        });
     }
 }
---
+++ app/src/main/java/com/mvvm/tvshows/utilities/BindingAdapters.java
@@ -0,0 +1,32 @@
+package com.mvvm.tvshows.utilities;
+
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+public class BindingAdapters {
+
+    @BindingAdapter("android:imageURL")
+    public static void setImageURL(ImageView imageView, String URL){
+        try{
+            imageView.setAlpha(0f);//zero f
+            Picasso.get().load(URL).noFade().into(imageView, new Callback() {
+                @Override
+                public void onSuccess() {
+                    imageView.animate().setDuration(300).alpha(1f).start();//one f
+
+                }
+
+                @Override
+                public void onError(Exception e) {
+
+                }
+            });
+        } catch (Exception ignored){
+
+        }
+    }
+}
